c921c9d151a5f3f7e2dd522462c491ac523a06b9
Ryan Gibson <ryan.alexander.gibson@gmail.com>
2025-03-15 23:35:21 -0400

    Fix missed args in .webp shim

--- matplotlib_lossless_webp.py
+++ matplotlib_lossless_webp.py
@@ -9,7 +9,7 @@ def patch():
         if isinstance(fname, str) and fname.lower().endswith('.webp'):
             # just guarding against non-str here to have same error behavior as matplotlib
             kwargs['pil_kwargs'] = {'lossless': True} | kwargs.get('pil_kwargs', {})
-            return original_savefig(self, fname, **kwargs)
+            return original_savefig(self, fname, *args, **kwargs)
         return original_savefig(self, fname, *args, **kwargs)
 
     plt.Figure.savefig = savefig
