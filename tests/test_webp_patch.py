11c66f67d00781deebd3843859aac2c73a42e1b5
Ryan Gibson <ryan.alexander.gibson@gmail.com>
2025-03-15 22:50:41 -0400

    Add tests that this improves matplotlib's exports

---
+++ tests/test_webp_patch.py
@@ -0,0 +1,40 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+def check_webp_matches_png():
+    """Check whether .webp and .png exports have identical image data."""
+    plt.figure()
+    plt.plot([1, 2, 3], [4, 5, 6])
+
+    plt.savefig('test_compare.webp')
+    plt.savefig('test_compare.png')
+    plt.close()
+
+    is_perfect_match = np.array_equal(
+        np.array(Image.open('test_compare.webp')),
+        # we need to ignore the (unused) PNG alpha layer here
+        np.array(Image.open('test_compare.png').convert("RGB"))
+    )
+
+    os.remove('test_compare.webp')
+    os.remove('test_compare.png')
+    return is_perfect_match
+
+
+def test_default_webp_does_not_match_png():
+    """
+    Test that matplotlib's default .webp exports are lower quality than .png.
+
+    If this fails, matplotlib's default has been changed to lossless, so this package has no effect.
+    """
+    assert not check_webp_matches_png()
+
+
+def test_patch_webp_matches_png():
+    """Test that .webp and .png exports have identical image data after our patch."""
+    import matplotlib_lossless_webp  # noqa, we actually depend on this test running second
+    assert check_webp_matches_png()
